05cd54a6203aae71d10d67b3a7698864eec676ee
Leticia Halana Ribeiro de Sousa <leticia.halana.tr@gmail.com>
2025-03-17 12:26:08 -0300

    feat: atualizada e renderizada a lista de amigos.

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ function adicionarAmigo() {
 
   if (nomeAmigo === "") {
     alert("Por favor, insira um nome.");
+  } else if (listaDeAmigos.includes(nomeAmigo)) {
+    alert("Este nome já foi adicionado.");
+    document.querySelector(".input-name").value = "";
   } else {
     listaDeAmigos.push(nomeAmigo);
+    atualizarListaDeAmigos();
     document.querySelector(".input-name").value = "";
   }
+}
 
-  console.log(listaDeAmigos);
+function atualizarListaDeAmigos() {
+  let renderListaDeAmigos = document.getElementById("listaAmigos");
+  renderListaDeAmigos.innerHTML = "";
+
+  for (let i = 0; i < listaDeAmigos.length; i++) {
+    let list = document.createElement("li");
+    list.innerHTML = listaDeAmigos[i];
+    renderListaDeAmigos.appendChild(list);
+  }
 }
